ef60b3d7a1640901ff9f30bb70da7a2af1d75144
Puleeno Nguyen <puleeno@gmail.com>
2026-08-02 22:34:13 +0700

    feat: add Composer autoloader files to the my-account extension library

---
+++ libs/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jankx/mya-account',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6fa9b16558c4082432888e95b867a258a495343f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jankx/mya-account' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6fa9b16558c4082432888e95b867a258a495343f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
