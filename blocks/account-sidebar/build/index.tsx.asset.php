3ee2422fd498aac0728231f1672011a0fa63f1f2
Puleeno Nguyen <puleeno@gmail.com>
2026-09-03 04:46:26 +0700

    build: update minified block scripts and asset versions for account sidebar and profile tab

--- blocks/account-sidebar/build/index.tsx.asset.php
+++ blocks/account-sidebar/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '0c3a856f0a0edb6ccaa3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '78b0b916d5406474d23f');
--- blocks/account-tab-profile/build/index.tsx.asset.php
+++ blocks/account-tab-profile/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '93e81a25d209303423b6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '133cb0419716710ba165');
