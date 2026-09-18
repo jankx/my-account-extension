31b237e69f0ce106b6839bb2cb1fe9c3e738c988
Puleeno Nguyen <puleeno@gmail.com>
2026-09-19 01:48:47 +0700

    feat: add account tab overview block with server-side rendering support

---
+++ blocks/account-tab-overview/build/index.tsx.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '0247c7a741c2cc68f73e');
---
+++ blocks/account-tab-overview/src/edit.tsx
@@ -0,0 +1,18 @@
+import { useBlockProps } from '@wordpress/block-editor';
+import ServerSideRender from '@wordpress/server-side-render';
+import { __ } from '@wordpress/i18n';
+
+export default function Edit({ attributes }) {
+    const blockProps = useBlockProps({
+        className: 'jankx-account-tab-overview is-editor-preview',
+    });
+
+    return (
+        <div {...blockProps}>
+            <ServerSideRender
+                block="jankx/account-tab-overview"
+                attributes={attributes}
+            />
+        </div>
+    );
+}
