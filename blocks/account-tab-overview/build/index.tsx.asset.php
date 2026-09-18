6a307e099670d8b097f9a7b59a8b52011bafef6c
Puleeno Nguyen <puleeno@gmail.com>
2026-09-19 03:25:21 +0700

    feat: update version for account tab overview block and fix build script reference

--- blocks/account-tab-overview/build/index.tsx.asset.php
+++ blocks/account-tab-overview/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '0247c7a741c2cc68f73e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'f99ec9de4d3cc609193f');
